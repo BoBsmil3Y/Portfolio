5265ba758d96ee2d8b6597022169f317a5ae78a1
BoBsmil3Y <bygamusic@gmail.com>
2019-10-18 00:00:45 +0200

    Beginning skills section & testing background section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+/*
+Navbar burger enable
+*/
 document.addEventListener('DOMContentLoaded', () => {
 
   // Get all "navbar-burger" elements
@@ -23,3 +26,28 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 });
+
+/*
+Show modal on button click
+*/
+
+$(".show-modal").click(function() {
+  $(".modal-" + $(this).attr("id")).addClass("is-active");
+});
+
+$(".to-close").click(function() {
+  $(".modal-" + $(this).attr("id")).removeClass("is-active");
+});
+
+
+/*
+Parallax background
+*/
+document.getElementById("body").onscroll = function myFunction() {  
+  var scrolltotop = document.scrollingElement.scrollTop;
+  var target = document.getElementById("has-background");
+  var xvalue = "center";
+  var factor = 0.5;
+  var yvalue = scrolltotop * factor;
+  target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+}
